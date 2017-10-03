f37a74bb740c5ed9d6a2a7c3a3df53a4718ab106
Rob Dey <zn.ninkovic@y7mail.com>
2017-10-03 07:40:54 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < Action Mailer: :Base
+    default to: 'zn.ninkovic@y7mail.com'
+  def contact_email(name,email,body)
+    @name= name
+    @email= email
+    @body= body
+    
+    mail(from: email,subject:'Contact Form Message')
+  end
+end
