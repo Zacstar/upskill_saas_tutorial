d9c3d1c7e309a2fa4384157628cbf2ce622cc3c6
Rob Dey <zn.ninkovic@y7mail.com>
2017-10-13 09:11:11 +0000

    Added master settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings= {
+    :port       => ENV['MAILGUN_SMTP_PORT'],
+    :adress     => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name  => ENV['MAILGUN_SMTP_LOGIN'],
+    :domain     => 'shrouded-cove-17130.herokuapp.com',
+    :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
