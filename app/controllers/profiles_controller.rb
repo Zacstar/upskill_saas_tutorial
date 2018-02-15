79abd8de67f5c08ae5922075f472ac09c4be466e
Rob Dey <zn.ninkovic@y7mail.com>
2018-02-15 03:12:11 +0000

    Updated edit button

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -26,6 +26,22 @@ class ProfilesController < ApplicationController
    @profile= @user.profile
   end
   
+  #PATCH to /users/:user_id/profile
+  def update
+    # Retreive the user from database
+    @user= User.find( params[:user_id] )
+    #Retreive that user's profile
+    @profile= @user.profile 
+    #Mass assign edited profile attributes and save (update)
+    if @profile.update_attributes(profile_params)
+      flash[:success]= "Profile Updated!"
+      #Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+  
 private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
